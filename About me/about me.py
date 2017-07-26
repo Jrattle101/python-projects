2aa8c109ee65f29c75af78f8522727ae0f141ade
Jason Adams <jason.r.adams@outlook.com>
2017-07-26 08:35:38 -0400

    Added first project
    added about me .py

---
+++ About me/about me.py
@@ -0,0 +1,22 @@
+print('Hello!')
+
+print('Here\s a picture of a dog')
+print(' o____ ')
+print('  |||| ')
+
+born=input('What year were you born?')
+born=int(born)
+age=2025-born
+print('In the year 2025 you\'ll be', age, 'years old!')
+      
+age=input('what is your age?')
+age=int(age)
+age=age*7
+print('If you were a dog, you\'d be', age, '!!')
+
+print('Here is a scarf:')
+print('~#' * 10)
+print('#-' * 10)
+print('Here is a wave')
+print('/\ ' * 10)
+print(' \/' * 10)
