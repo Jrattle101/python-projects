980980a45bdb02d7db21c35fff5351592629d75f
Jason Adams <jason.r.adams@outlook.com>
2017-07-27 15:28:23 -0400

    new
    new

---
+++ Cypher/Cypher.py
@@ -0,0 +1,7 @@
+alphabet='abcdefghijklmnopqrstuvwxyz'
+key=3
+
+character=input('Please enter a character:')
+
+position=alphabet.find(character)
+print(position)
---
+++ number count.py
@@ -0,0 +1,9 @@
+for i in range(1, 101):
+    if i % 15==0:
+        print('rightpoint')
+    elif i % 3==0:
+        print('right')
+    elif i % 5==0:
+        print('point')
+    else:
+        print (i)
