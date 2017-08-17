fc2021cedb5082f42ab08e8cb76cdcb625ea6337
Jason Adams <jason.r.adams@outlook.com>
2017-08-16 20:19:03 -0400

    Number sort
    number sort app

---
+++ num.py
@@ -0,0 +1,24 @@
+##n = int(input('enter a number'))
+##if n % 2==0 and n in range(1,5):
+##    print('Not Weird')
+##elif n % 2==0 and n in range(6,21):
+##    print('Weird')
+##elif n % 2==0 and n > 20:
+##    print('Not Weird')
+##else:
+##    print('Weird')
+
+
+##n = int(input().strip())
+##check = {True: "Not Weird", False: "Weird"}
+##
+##print(check[
+##        n%2==0 and (
+##            n in range(2,6) or 
+##            n > 20)
+##    ])
+
+a=int(input())
+b=int(input())
+
+print (a+b)
